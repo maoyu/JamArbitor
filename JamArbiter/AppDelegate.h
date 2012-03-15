6f4cfa709381e88ea13a000bea4f1986cdf1da54
Liu WanWei <liuwanwei@gmail.com>
2012-03-15 14:24:55 +0800

    Initial Commit

---
+++ JamArbiter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JamArbiter
+//
+//  Created by Liu Wanwei on 12-3-15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ JamArbiter/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  JamArbiter
+//
+//  Created by Liu Wanwei on 12-3-15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
