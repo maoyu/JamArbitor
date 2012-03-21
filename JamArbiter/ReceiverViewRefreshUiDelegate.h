a8ca2b1536e95979143391f289d471e920726148
maoyu <maoyu417@163.com>
2012-03-21 11:38:45 +0800

    添加新文件，用于接受者ui刷新

---
+++ JamArbiter/ReceiverViewRefreshUiDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ReceiverViewRefreshUiDelegate.h
+//  JamArbiter
+//
+//  Created by maoyu on 12-3-20.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@protocol ReceiverViewRefreshUiDelegate
+-(void) refreshUI;
+
+@end
